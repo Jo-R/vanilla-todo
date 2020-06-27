dba1aa80139607d9fcf2e7035153f095c368c743
Jo-R <jo.rigg8@gmail.com>
2020-06-27 08:56:03 +0100

    can delete items

--- scripts/todo-logic.js
+++ scripts/todo-logic.js
@@ -2,7 +2,7 @@ class ToDoLogic {
   constructor() {
     this.addHandler = this.addHandler.bind(this);
     this.confirmAddHandler = this.confirmAddHandler.bind(this);
-
+    this.deleteHandler = this.deleteHandler.bind(this);
     this.addBtn = document.getElementById("add-btn");
     this.addBtn.addEventListener("click", this.addHandler);
     this.addBtn = document.getElementById("confirm-add");
@@ -24,32 +24,52 @@ class ToDoLogic {
     document.getElementById("confirm-add").classList.remove("hidden");
   }
 
-  createListItem(newItem, index) {
+  deleteHandler(evt) {
+    const idToDelete = document.getElementById(evt.target.id).dataset.id;
+    const newList = JSON.parse(window.localStorage.getItem("todo")).filter(
+      (item) => item.id != idToDelete
+    );
+    const toRemove = document.getElementById(`item-${idToDelete}`);
+    toRemove.parentNode.removeChild(toRemove);
+    localStorage.setItem("todo", JSON.stringify(newList));
+    newList.map((item) => {
+      this.createListItem(item);
+    });
+  }
+
+  createListItem(item, index) {
     var div = document.createElement("DIV");
     div.classList.add("item-container");
+    div.setAttribute("id", `item-${item.id}`);
     var para = document.createElement("P");
-    var text = document.createTextNode(newItem);
+    var text = document.createTextNode(item.value);
     div.appendChild(para);
     para.appendChild(text);
     var btn = document.createElement("BUTTON");
+    btn.addEventListener("click", this.deleteHandler);
     btn.innerText = "Delete";
-    btn.setAttribute("id", `delete-${index}`);
+    btn.setAttribute("id", `delete-${item.id}`);
+    btn.setAttribute("data-id", item.id);
     div.appendChild(btn);
     document.getElementById("todo-list").appendChild(div);
   }
 
   confirmAddHandler(evt) {
     const input = document.getElementById("new-item");
-    const newItem = input.value;
+    const newItemText = input.value;
     input.value = "";
     input.classList.add("hidden");
     document.getElementById("confirm-add").classList.add("hidden");
     const existingStoredItems = JSON.parse(window.localStorage.getItem("todo"));
-    existingStoredItems.push(newItem); // TODO this needs to be an object really and can probs spread or what have you, needs an id and the delete button needs linking to that so can delete item
-    existingStoredItems.map((item, idx) => {
-      this.createListItem(item, idx);
+    const newItem = {
+      id: existingStoredItems.length + 1,
+      value: newItemText,
+    };
+    const newList = [...existingStoredItems, newItem];
+    localStorage.setItem("todo", JSON.stringify(newList));
+    newList.map((item) => {
+      this.createListItem(item);
     });
-    localStorage.setItem("todo", JSON.stringify(existingStoredItems));
   }
 }
 
